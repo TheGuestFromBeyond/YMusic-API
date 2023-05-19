75f91652f242a6282cec5d5cb58eeb5b5bf049a7
TheGuestFromBeyond <theguestfrombeyond@protonmail.com>
2023-05-19 11:20:19 +0300

    first commit

---
+++ src/main/java/com/beyond/ymusicapi/YmusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.ymusicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmusicApiApplication.class, args);
+	}
+
+}
