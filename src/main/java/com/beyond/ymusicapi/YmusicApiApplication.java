37ce5d8a6978da0e9483e8df952847c995b6701e
TheGuestFromBeyond <theguestfrombeyond@protonmail.com>
2023-05-22 21:05:00 +0300

    Add beans for common components

--- src/main/java/com/beyond/ymusicapi/YmusicApiApplication.java
+++ src/main/java/com/beyond/ymusicapi/YmusicApiApplication.java
@@ -1,13 +1,38 @@
 package com.beyond.ymusicapi;
 
+import com.beyond.ymusicapi.request.common.Client;
+import com.beyond.ymusicapi.request.common.Context;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.OkHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class YmusicApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YmusicApiApplication.class, args);
+	}
+
+	@Bean
+	public Client getClient() {
+		return new Client();
+	}
+
+	@Bean
+	public Context getContext() {
+		return new Context(getClient());
+	}
+
+	@Bean
+	public OkHttpClient getOkHttpClient() {
+		return new OkHttpClient();
+	}
+
+	@Bean
+	public ObjectMapper getObjectMapper() {
+		return new ObjectMapper();
 	}
 
 }
