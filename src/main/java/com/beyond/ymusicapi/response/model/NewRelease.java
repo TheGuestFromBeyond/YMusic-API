e002bc340c34fc3939f763fb046a6642e051fe97
TheGuestFromBeyond <theguestfrombeyond@protonmail.com>
2023-05-23 21:14:51 +0300

    Add a model class for new release entity

---
+++ src/main/java/com/beyond/ymusicapi/response/model/NewRelease.java
@@ -0,0 +1,31 @@
+package com.beyond.ymusicapi.response.model;
+
+public class NewRelease {
+    private String releaseName;
+    private String artistName;
+    private String artistId;
+
+    public String getReleaseName() {
+        return releaseName;
+    }
+
+    public void setReleaseName(String releaseName) {
+        this.releaseName = releaseName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+}
