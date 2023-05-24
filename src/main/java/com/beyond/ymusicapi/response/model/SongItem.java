4b9b33c716521ca36e0529371769ef37afc4f2fa
TheGuestFromBeyond <theguestfrombeyond@protonmail.com>
2023-05-24 18:50:02 +0300

    Use a model for a song item

---
+++ src/main/java/com/beyond/ymusicapi/response/model/SongItem.java
@@ -0,0 +1,31 @@
+package com.beyond.ymusicapi.response.model;
+
+public class SongItem {
+    private String songName;
+    private String songId;
+    private String playlistId;
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getSongId() {
+        return songId;
+    }
+
+    public void setSongId(String songId) {
+        this.songId = songId;
+    }
+
+    public String getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(String playlistId) {
+        this.playlistId = playlistId;
+    }
+}
